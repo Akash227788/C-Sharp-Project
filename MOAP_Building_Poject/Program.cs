8432746ca3625bb184907be41a2935e1eb7115ad
akash joseph <akashfrancis274@gmail.com>
2024-07-12 22:02:23 +0530

    Add files via upload

---
+++ MOAP_Building_Poject/Program.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using MOAP_Library;
+
+//  implementing inheritance by inheriting interface and abstract classes
+
+
+public class Colony 
+{
+    // create objects of 1Bhk and 2Bhk from interface. use school, mall and tech park objects
+    public OneBhk SingleHomes { get; set; }
+    public TwoBhk FamilyHomes { get; set; }
+    public Villa LuxHome { get; set; }
+    public School Patel { get; set; }
+    public Mall Bharathiya { get; set; }
+    public TechPark Manyata { get; set; }
+    
+    // normal constructor
+    public Colony(OneBhk oneBhk, TwoBhk twoBhk, Villa villa, Mall mall)
+    {   
+    }
+    // parameterized constructor
+    public Colony(OneBhk oneBhk, TwoBhk twoBhk, Villa villa, Mall mall, TechPark tpark)
+    {
+        this.SingleHomes = oneBhk;
+        this.FamilyHomes = twoBhk;
+        this.LuxHome = villa;
+        this.Bharathiya = mall;
+        this.Manyata = tpark;
+    }
+
+   
+
+
+}
+
+namespace MOAP_Building_Poject
+{
+    internal class Program
+    {
+        // Writing the linq query to search school from the three syllabus options
+        public static void Search(List<MOAP_Library.School> InstitutionList, string key)
+        {
+            // linq 
+            var SearchKey = from syllabus in InstitutionList
+                            where syllabus.Affiliation == key
+                            select syllabus;
+
+            // output
+            int count = 0;
+            Console.WriteLine("The found schools are: ");
+            foreach (var item in SearchKey)
+            {
+                count++;
+                Console.WriteLine($"{count}: {item.Affiliation}");
+            }
+        }
+        
+
+        // Writing a function to use Lambda function
+        public static void OrderSchool(List<MOAP_Library.School> InstitutionList)
+        {
+            // order by asc 
+            var OrderAsc = InstitutionList.OrderBy(x=> x.Owner).ToList();
+
+            Console.WriteLine("The List is Ordered by Ascending Order ");
+            
+            int c1 = 0;
+            foreach (var item in OrderAsc)
+            {
+                c1++;
+                Console.WriteLine($"{c1}: {item.Owner} "); // {item.ParentConstructor.DevPlace}
+            }
+
+            // order by desc
+            var OrderDesc = InstitutionList.OrderByDescending(x=> x.Owner).ToList();
+
+            Console.WriteLine("The List is Ordered by Descending Order ");
+            int c2 = 0;
+            foreach (var item in OrderDesc)
+            {
+                c2++;
+                Console.WriteLine($"{c2}: {item.Owner} "); //and {item.ParentConstructor.DevPlace}
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            //MOAP_Library.Developer d1 = new Developer("Godrej", "Bengaluru","560001");
+            List<MOAP_Library.Developer> BuilderList = new List<Developer> {
+                new Developer("Godrej", "Bengaluru","560001"),
+                new Developer("Tata Projects", "Mumbai", "400001"),
+                new Developer("Larsen & Toubro", "Mumbai", "400050"),
+                new Developer("Lodha Group", "Pune", "411001"),
+                new Developer("Reliance Infrastructure", "Delhi", "110001"),
+                new Developer("Sadbhav Engineering", "Punjab", "141001"),
+                new Developer("Punj Lloyd", "Haryana", "133001"),
+            };
+            
+            //MOAP_Library.Building b1 = new MOAP_Library.School(8000, 4, "St Josephs", 8000, d1,"State");
+            List<MOAP_Library.School> InstitutionList = new List<MOAP_Library.School>
+            {
+                new MOAP_Library.School(8000, 4, "St Josephs", 8000, BuilderList[1], "State"),
+                new MOAP_Library.School(6000, 3, "ABC School", 6000, BuilderList[0], "CBSE"),
+                new MOAP_Library.School(7000, 5, "XYZ School", 7000, BuilderList[1], "ICSE"),
+                new MOAP_Library.School(5000, 5, "Maple High School", 5000, BuilderList[0], "CBSE"),
+                new MOAP_Library.School(9000, 6, "Greenfield Academy", 9000, BuilderList[3], "ICSE"),
+                new MOAP_Library.School(7500, 4, "Sunshine Public School", 7500, BuilderList[2], "State"),
+                new MOAP_Library.School(5500, 3, "Rosewood International", 5500, BuilderList[3], "CBSE"),
+                new MOAP_Library.School(6800, 5, "Oakridge High", 6800, BuilderList[4], "ICSE"),
+                new MOAP_Library.School(7200, 4, "Blossom School", 7200, BuilderList[5], "State"),
+                new MOAP_Library.School(6200, 3, "Sunflower Academy", 6200, BuilderList[6], "CBSE"),
+                new MOAP_Library.School(8000, 5, "Lotus High School", 8000, BuilderList[3], "ICSE"),
+                new MOAP_Library.School(6700, 4, "Rainbow Public School", 6700, BuilderList[5], "State"),
+                
+            };
+            InstitutionList[0].Lighting();
+            InstitutionList[0].Pool();
+            InstitutionList[0].TotalCost();
+
+            // now add , lambda, linq,  
+            string key1 = "State";
+            string key2 = "ICSE";
+            string key3 = "CBSE";
+            Search(InstitutionList, key1);
+            Search(InstitutionList, key2);
+            Search(InstitutionList, key3);
+
+            // exception example with building class
+            MOAP_Library.Developer d1 = new Developer("Godrej", "Bengaluru","560001");
+            //Building b1 = new School(3000, 2, "St Josephs", 3000, d1, "State");
+
+            // function to implement lambda function
+            // Orders by asc and desc and stores in a list
+            OrderSchool(InstitutionList);
+
+            // Creating the Colony objects 
+            Developer godrej = new Developer("Godrej", "Bengaluru", "560001");
+            OneBhk studioApart = new OneBhk();
+            TwoBhk famApart = new TwoBhk();
+            Villa luxHome = new Villa();
+            Mall BharathiyaMall = new Mall(9000, 7,"Shetty's", 8000, godrej, 12);
+            TechPark Manyata = new TechPark(8000, 4, "Garuda", 5000, godrej, 7);
+            Colony FrenchColony = new Colony(studioApart, famApart, luxHome, BharathiyaMall, Manyata);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MOAP_Library/Building.cs
@@ -0,0 +1,248 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MOAP_Library
+{
+    // Inheritance has-a rel object Parent construction company for the property
+    public class Developer
+    {
+        public string DevName { get; set; }
+        public string DevPlace { get; set; }
+        public string Pincode { get; set; }
+        public Developer(string name, string place, string pincode)
+        {
+            this.DevName = name;
+            this.DevPlace = place;
+            this.Pincode = pincode;
+        }
+    }
+
+    public abstract class Building
+    {
+        // data members
+        public double CostPerSqft { get; set; }
+        public int Floor { get; set; }
+        public string Owner { get; set; }
+        public int Size { get; set; }
+
+        // adding the object dev name
+        public Developer ParentConstructor { get; set; }
+
+
+        // constructors
+        public Building()
+        {
+
+        }
+
+        public Building(int cost, int floor, string owner, int size, Developer BuildingParent)
+        {
+            this.CostPerSqft = cost;
+            this.Floor = floor;
+            this.Owner = owner;
+            this.Size = size;
+        }
+
+        // abstract member functions
+        public abstract void Pool();
+        public abstract void TotalCost();
+        public abstract void Lighting();
+
+
+    }
+
+    // School child class using Building abstract class
+    public class School : Building
+    {
+        // data members
+        public string Affiliation { get; set; }
+
+        
+
+        public School(int cost, int floor, string owner, int size, Developer BuildingParent, string affiliate) : base(cost, floor, owner, size, BuildingParent)
+        {
+            // Implementing exceptions here
+            if (CostPerSqft < 5000) // cost should not be less than 10k
+            {
+                throw new ArithmeticException("Please give valid Cost");
+            }
+            if (Floor < 3) // min 3 floors for a School
+            {
+                throw new ArithmeticException("Please enter more than 3 floor");
+            }
+            if (Owner == null)
+            {
+                throw new ArgumentException("Please give a valid Owner Name");
+            }
+            if (Size < 5000) // Throw exception if size is less than 5000 sqft
+            {
+                throw new ArithmeticException("Please give valid Cost");
+            }
+            this.Affiliation = affiliate;
+        }
+
+
+        public override void Pool()
+        {
+            // 1 pool per 1000sqft for schools
+            int noOfPools = Size / 1000;
+            System.Console.WriteLine($"There are {noOfPools} pools in {Owner} School");
+        }
+
+        public override void Lighting()
+        {
+            // 5 per 1000 sqft
+            int totLight = Size / 200;
+            System.Console.WriteLine($"Total Lights required is: {totLight}");
+        }
+
+        public override void TotalCost()
+        {
+            double totCost = Size * CostPerSqft;
+            System.Console.WriteLine($"Cost for the construction of {Owner} is: {totCost}");
+        }
+    }
+
+    // Mall child class using Building abstract class
+    public class Mall : Building
+    {
+        public int Brands { get; set; }
+
+        // Throw exception if size is less than 8000 sqft
+
+        public Mall(int cost, int floor, string owner, int size, Developer BuildingParent, int brands) : base(cost, floor, owner, size, BuildingParent)
+        {
+            this.Brands = brands;
+        }
+
+
+        public override void Pool()
+        {
+            // 1 pool per 2000sqft for Malls
+            int noOfPools = Size / 2000;
+            System.Console.WriteLine($"There are {noOfPools} in {Owner} Mall");
+        }
+
+        public override void Lighting()
+        {
+            // 10 per 1000 sqft
+            int totLight = Size / 100;
+            System.Console.WriteLine($"Total Lights required is: {totLight}");
+        }
+
+        public override void TotalCost()
+        {
+            // Multiplier for Mall as cost is more
+            double totCost = Size * CostPerSqft + (CostPerSqft * Size * 0.2);
+            System.Console.WriteLine($"Cost for the construction of {Owner} is: {totCost}");
+        }
+    }
+
+    // TechPark child class using Building abstract class
+
+    public class TechPark : Building
+    {
+        public int Companies { get; set; }
+
+        // Throw exception if size is less than 20000 sqft
+
+        public TechPark(int cost, int floor, string owner, int size, Developer BuildingParent, int companies) : base(cost, floor, owner, size, BuildingParent)
+        {
+            this.Companies = companies;
+        }
+
+
+        public override void Pool()
+        {
+            // 1 pool per 4000sqft for Tech Parks
+            int noOfPools = Size / 4000;
+            System.Console.WriteLine($"There are {noOfPools} in {Owner} Mall");
+        }
+
+        public override void Lighting()
+        {
+            // 5 per 1000 sqft
+            int totLight = Size / 200;
+            System.Console.WriteLine($"Total Lights required is: {totLight}");
+        }
+
+        public override void TotalCost()
+        {
+            // Multiplier for Tech park as cost is more and sez reduction
+            double totCost = Size * CostPerSqft + (CostPerSqft * Size * 0.7) - (Size * 200);
+            System.Console.WriteLine($"Cost for the construction of {Owner} is: {totCost}");
+        }
+    }
+
+    public class Villa: Building
+    {
+        public override void Pool()
+        {
+            Console.WriteLine("Villa has pool");
+        }
+
+        public override void Lighting()
+        {
+            Console.WriteLine("Villa has pool");
+        }
+
+        public override void TotalCost()
+        {
+            Console.WriteLine("Villa has pool");
+        }
+    }
+
+    // interface example for Building idea
+    // interface ideas -> 1bhk, kitchen, 
+
+    // Interface 
+    public interface IResidential_Essentials
+    {
+        // interface method declarations
+        void Kitchen();
+        void Restroom();
+        void Bedroom();
+    }
+
+
+
+    // interface implementations
+    // 1BHK house
+    public class OneBhk : IResidential_Essentials
+    {
+        public void Kitchen()
+        {
+            System.Console.WriteLine("One Kitchen");
+        }
+        public void Restroom()
+        {
+            System.Console.WriteLine("One Restroom");
+        }
+        public void Bedroom()
+        {
+            System.Console.WriteLine("One Master Bedroom");
+        }
+    }
+
+    // 2 BHK house
+    public class TwoBhk : IResidential_Essentials
+    {
+        public void Kitchen()
+        {
+            System.Console.WriteLine("Two Kitchen");
+        }
+        public void Restroom()
+        {
+            System.Console.WriteLine("Two Restroom");
+        }
+        public void Bedroom()
+        {
+            System.Console.WriteLine("Two Master Bedroom");
+        }
+    }
+
+
+}
